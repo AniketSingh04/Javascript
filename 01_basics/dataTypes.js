623d4302432d65693079c1d2ac5f9fecbbdb4f41
Aniket Raj Singh <92619734+AniketSingh04@users.noreply.github.com>
2024-04-07 17:28:12 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+ 
+//alert(3+3); we are using node not browser
+
+//ECMA standards and MDN docs for Javascript standards
+
+//numebr => 0-9
+//bigint => for large nos.
+//string => "abscd33"
+//boolean => true / false
+//null => standalone value(represents empty val)
+//undefined => not defined
+//symbol => uniqueness
+
+let name = "aniket"
+let age = 22
+let state = true
+let gf = null
+
+console.table([name, age, state, gf])
+
+console.log(typeof name)
+console.log(typeof gf)
+console.log(typeof undefined)
