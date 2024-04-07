f712e048159c9bee6e6b9eaa892ef5ecf705e57d
Aniket Raj Singh <92619734+AniketSingh04@users.noreply.github.com>
2024-04-07 17:55:50 +0000

    Conersion of data types

---
+++ 01_basics/conversions.js
@@ -0,0 +1,38 @@
+// let score = "33abc"
+// console.log(typeof(score))
+
+// let value = Number(score)
+// console.log(typeof(value))
+// console.log(value)
+
+// let score2 = null
+// console.log(score2)
+
+// value = Number(score2)
+// console.log(value)
+// console.log(typeof(value2))
+
+/* 
+"33" => 33
+"33abc" => NaN
+true => 1; false => 0;
+*/
+
+// let LoggedIn = " "
+// let boolVal = Boolean(LoggedIn)
+// console.log(boolVal);
+// console.log(typeof(boolVal));
+/*
+1 -> true
+0 -> false
+"" -> false
+"<any string>" -> true
+*/
+
+let someNumber = 45
+let str = String(someNumber)
+console.log(str);
+console.log(typeof(str));
+/*
+45 => "45"
+*/
