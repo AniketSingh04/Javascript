00c01e4da5e6f287f041062e52023940bde0753a
Aniket Raj Singh <92619734+AniketSingh04@users.noreply.github.com>
2024-04-07 18:47:31 +0000

    comaparisions

---
+++ 01_basics/comparisions.js
@@ -0,0 +1,21 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2>=1)
+// console.log(2<=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log("2">1);
+// console.log("01"<5);
+
+// console.log(null > 0); //comparision operators convert
+// console.log(null < 0); //null into 0 and then check
+// console.log(null >= 0); // with other value; here 0
+// console.log(null == 0); //while  equality operator dont 
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+
+// === (strict check)-> checks the datatype of both values and proceeds if both are same
+console.log("2"===2);
