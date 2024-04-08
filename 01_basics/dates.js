b01c1b55952bf7cb687956e7ec92e27830dc8d25
Aniket Raj Singh <92619734+AniketSingh04@users.noreply.github.com>
2024-04-08 18:28:34 +0000

    dates and timestamps in depth

---
+++ 01_basics/dates.js
@@ -0,0 +1,42 @@
+// Date
+
+// let myDate = new Date();
+// console.log(myDate);
+
+// let Date1 = myDate.toString();
+// let Date2 = myDate.toDateString();
+// let Date3 = myDate.toISOString(); 
+// let Date4 = myDate.toJSON();
+// let Date5 = myDate.toLocaleDateString('en-IN');
+// let Date6 = myDate.toLocaleString('ar-EG');
+
+// console.table([Date1, Date2, Date3, Date4, Date5, Date6]);
+
+
+
+// let createDate = new Date(2023, 5, 23) //month starts with 0 in Date in js
+// console.log(createDate.toDateString());
+
+// console.log(Date.now());  // in ms
+// console.log(createDate.getTime());
+// console.log(Math.round(Date.now()/1000)); in seconds
+
+// let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getDay());
+// console.log(newDate.getHours());
+// console.log(newDate.getMonth() + 1);
+
+// const date = new Date();
+
+// // Customize date format
+// const options = {
+//   weekday: 'long', // Full weekday name (e.g., "Monday")
+//   year: 'numeric', // Full numeric representation of the year (e.g., "2024")
+//   month: 'long', // Full month name (e.g., "January")
+//   day: 'numeric' // Day of the month (e.g., "1")
+// };
+
+// const customDateString = date.toLocaleString('default', options);
+// console.log(customDateString);
+
