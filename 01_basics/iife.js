66f25bbefba7145816224d97c8e4d0f502639918
Aniket Raj Singh <92619734+AniketSingh04@users.noreply.github.com>
2024-04-09 21:10:42 +0000

    Immediately Invoked Function Expression

---
+++ 01_basics/iife.js
@@ -0,0 +1,8 @@
+//Immediately Invoked Function Expression (IIFE)
+
+//global scope k pollution ko htane k liye hmne y use kiya
+//synatx :- (<function-definition and declaration>) () ;
+
+(function Database(){
+    console.log(`Database connected`);
+}) ();
