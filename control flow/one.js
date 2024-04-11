b07a842fdfba975f490e9b1b45c3ecb01e62d35d
Aniket Raj Singh <92619734+AniketSingh04@users.noreply.github.com>
2024-04-11 10:14:00 +0000

    Control flow Structure

---
+++ control flow/one.js
@@ -0,0 +1,26 @@
+//following are the topics in control flow:-
+
+// if-else statements
+
+//switch statements
+
+//assignment operators
+
+//strcitly equal and not strictly equal
+
+//string k andr koi bhi value true hoti hai 
+
+//true values - true, "0", "false", " ", [], {}, fucntion(){}
+
+//false values - false, 0, "", [], {}, null, undefined, NaN, BigInt (0n)
+
+//logical operators - &&, ||, !
+
+//Nullish Coalescing Operator - ?? : null or undefined
+
+//ternary operator - (condition) ? (true) : (false)
+
+//nullish coalescing operator - (condition) ?? (default value)
+
+
+//read the above topics from mdn js doc
