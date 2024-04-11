1a27e2d5ed5463013b267001f6d1c9f61c045ec8
Aniket Raj Singh <92619734+AniketSingh04@users.noreply.github.com>
2024-04-11 10:45:58 +0000

    while loop

---
+++ Iterations/while.js
@@ -0,0 +1,16 @@
+//while loop
+
+// let i =5
+// while (i>1) {
+//     console.log(i);
+//     i--;   
+// }
+
+// let arr = [1,2,3,45];
+// let j = 0 
+// while (j<arr.length) {
+//     console.log(arr[j]);
+//     j++;
+// }
+
+
